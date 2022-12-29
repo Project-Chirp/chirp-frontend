aa6b1ad95dba4706269f4e6eb5bae91d3c1f0557
NasihNazeem <nasihnazeem@gmail.com>
2022-12-28 18:35:58 -0700

    Added ThemeProvider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 //import Welcome from "./pages/Welcome.tsx";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@material-ui/core'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#22AA6F'
+    },
+    secondary: {
+      main: '#212529'
+    }
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-          {/* <Route path="/" element={<Welcome />} /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Register />} />
-      </Routes>
+      <ThemeProvider>
+        <Routes>
+            {/* <Route path="/" element={<Welcome />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Register />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
