26099743a8497bc0403497e0751660d9d256c3cd
christopherkinyua <christopher.kinyua@ucalgary.ca>
2023-01-05 19:31:21 -0700

    Merging

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -6,49 +6,51 @@ import { Button, Typography } from "@mui/material";
 
 import "../Styles/Messages.css";
 import DMSearchBar from "../components/DMSearchBar";
-import NavBar from "../components/NavBar";
+import PageWrapper from "./PageWrapper";
 
 const Messages = () => {
   return (
     <>
-      {/* <NavBar /> */}
-      <div>
-        <div className="Messagesflexbox">
-          <h1 className="Messagesheader">Messages</h1>
+      <PageWrapper>
+        <div>
+          <div className="Messagesflexbox">
+            <h1 className="Messagesheader">Messages</h1>
 
-          <IconButton>
-            <ChatIcon className="ChatIcon" />
-          </IconButton>
+            <IconButton>
+              <ChatIcon className="ChatIcon" />
+            </IconButton>
+          </div>
+
+          <DMSearchBar />
         </div>
 
-        <DMSearchBar />
-      </div>
+        <div>
+          <h1 className="title">Select a Message!</h1>
+          <Typography className="messagethread">
+            Select an already existing mesage thread or start an entirely new
+            one!
+          </Typography>
 
-      <div>
-        <h1 className="title">Select a Message!</h1>
-        <Typography className="messagethread">
-          Select an already existing mesage thread or start an entirely new one!
-        </Typography>
-
-        <div className="MessagesButton">
-          <Button
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-              margin: 10,
-              backgroundColor: "#22AA6F",
-              borderRadius: 10,
-            }}
-            size={"large"}
-            sx={{ fontFamily: "Inter" }}
-            variant="contained"
-            /* Might have to change this */
-            type="submit"
-          >
-            New Message
-          </Button>
+          <div className="MessagesButton">
+            <Button
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                margin: 10,
+                backgroundColor: "#22AA6F",
+                borderRadius: 10,
+              }}
+              size={"large"}
+              sx={{ fontFamily: "Inter" }}
+              variant="contained"
+              /* Might have to change this */
+              type="submit"
+            >
+              New Message
+            </Button>
+          </div>
         </div>
-      </div>
+      </PageWrapper>
     </>
   );
 };
