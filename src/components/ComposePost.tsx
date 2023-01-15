cc2e4b0d10154f5f9ff5601f93e4dab78a2dbdce
Dennull <lieudennis7@gmail.com>
2023-01-15 15:13:49 -0700

    Compose post component styling

--- src/components/ComposePost.tsx
+++ src/components/ComposePost.tsx
@@ -1,19 +1,29 @@
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
 import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 import EmojiEmotionsOutlinedIcon from "@mui/icons-material/EmojiEmotionsOutlined";
 
 const ComposePost = () => {
   return (
-    <Box sx={{ display: "flex", padding: 2 }}>
-      <AccountCircle fontSize="large" sx={{ paddingRight: 1 }} />
-      <Box>
+    <Box sx={{ display: "flex", padding: 3 }}>
+      <Box sx={{ paddingRight: 1.5 }}>
+        <Avatar />
+      </Box>
+      <Box sx={{ minWidth: 250 }}>
         <TextField
+          fullWidth
           id="standard-multiline-static"
           hiddenLabel
           multiline
           variant="standard"
           placeholder="What's happening?"
+          sx={{ paddingBottom: 1 }}
         />
         <Box
           sx={{
@@ -30,7 +40,7 @@ const ComposePost = () => {
               <EmojiEmotionsOutlinedIcon />
             </IconButton>
           </Stack>
-          <Button variant="contained" size="small">
+          <Button variant="contained" size="small" sx={{ borderRadius: 10 }}>
             Post
           </Button>
         </Box>
