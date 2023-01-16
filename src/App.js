cc817f390291b1088145c644461216498ec271f5
AnindaS13 <73033758+AnindaS13@users.noreply.github.com>
2023-01-15 19:11:30 -0700

    In progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,21 @@
+import PageWrapper from "./PageWrapper";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <PageWrapper>
+        <ArrowBackIcon style={{ padding: 30 }} />
+        <h1>User Profile</h1>
+      </PageWrapper>
+      <img
+        src={process.env.PUBLIC_URL + "/temp_banner.jpg"}
+        alt="temp"
+        width="80%"
+        height="200px"
+      ></img>
+    </div>
+  );
+};
+
+export default ProfilePage;
