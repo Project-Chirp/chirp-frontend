cee32f3ad66255aa3c8101f9c1603e2b44aa70f8
NasihNazeem <nasihnazeem@gmail.com>
2023-01-23 23:40:44 -0700

    Worked on palette, typography and components

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,52 @@
+import { Avatar, Link } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Button, Paper } from "@mui/material";
+import Popover from "@mui/material/Popover";
 import React from "react";
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  console.log(anchorEl);
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  console.log(open);
+
+  return (
+    <Paper>
+      <Button onClick={handleClick}>
+        <Avatar />
+        <Typography sx={{ padding: 1.5 }}>Buzz</Typography>
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+      >
+        <Link>Log Out</Link>
+      </Popover>
+    </Paper>
+  );
 };
 
 export default Welcome;
