09ed7425e30171a46cc9aa97370b900ca8556053
NasihNazeem <nasihnazeem@gmail.com>
2023-02-14 12:48:53 -0700

    Removed .tsx from import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Welcome from "./pages/Welcome";
-import Timeline from "./pages/Timeline.tsx";
+import Timeline from "./pages/Timeline";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
