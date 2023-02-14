504131763d0caa4e64542c4066c8e29bc6167cd7
NasihNazeem <nasihnazeem@gmail.com>
2023-02-14 13:44:38 -0700

    test

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,25 +1,11 @@
 import { Box } from "@mui/material";
-import { Stack } from "@mui/system";
 import ComposePost from "../components/ComposePost";
-import PostList from "../components/Posts/PostList";
-
-const styles = {
-  root: {
-    width: "100%",
-  },
-  postListContainer: { padding: 2, width: "50%" },
-};
 
 const Timeline = () => {
   return (
-    <Stack direction="row" sx={styles.root}>
-      <Box>
-        <ComposePost />
-      </Box>
-      <Box sx={styles.postListContainer}>
-        <PostList />
-      </Box>
-    </Stack>
+    <Box>
+      <ComposePost />
+    </Box>
   );
 };
 
