7017b78e18b533c0f72b3cf0b51f67afd331f92b
NasihNazeem <nasihnazeem@gmail.com>
2023-02-14 13:09:28 -0700

    Reverted

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,11 +1,21 @@
 import { Box } from "@mui/material";
-import ComposePost from "../components/ComposePost";
+import { Stack } from "@mui/system";
+import PostList from "../components/Posts/PostList";
+
+const styles = {
+  root: {
+    width: "100%",
+  },
+  postListContainer: { padding: 2, width: "50%" },
+};
 
 const Timeline = () => {
   return (
-    <Box>
-      <ComposePost />
-    </Box>
+    <Stack direction="row" sx={styles.root}>
+      <Box sx={styles.postListContainer}>
+        <PostList />
+      </Box>
+    </Stack>
   );
 };
 
