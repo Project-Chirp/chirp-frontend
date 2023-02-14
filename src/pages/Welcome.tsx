17a9c5e8f82e91abdedbf7c6031e54d8f0580229
NasihNazeem <nasihnazeem@gmail.com>
2023-02-14 12:45:41 -0700

    Reverted back to old commit

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <Box
+      zIndex={2}
+      display={"flex"}
+      flexDirection={"column"}
+      maxWidth={400}
+      alignItems={"center"}
+      justifyContent={"center"}
+      margin={"auto"}
+      marginTop={5}
+      padding={3}
+      width={1000}
+    >
+      <Typography
+        variant="h1"
+        textAlign={"center"}
+        fontWeight={700}
+        fontSize={32}
+        paddingTop={10}
+        paddingBottom={5}
+      >
+        Welcome to Tweeter
+      </Typography>
+      <img
+        width={250}
+        height={250}
+        src={process.env.PUBLIC_URL + "/logojade.png"}
+        alt="Logo"
+      />
+      <Button
+        size={"large"}
+        sx={{
+          marginTop: 3,
+          marginBottom: 2,
+          borderRadius: 10,
+          width: 253.4,
+        }}
+        color="primary"
+        variant="contained"
+      >
+        Take me to Tweeter
+      </Button>
+      <Button
+        size={"large"}
+        sx={{
+          marginTop: 3,
+          marginBottom: 2,
+          borderRadius: 10,
+          width: 253.4,
+        }}
+        color="primary"
+        variant="contained"
+      >
+        Log out
+      </Button>
+    </Box>
+  );
 };
 
 export default Welcome;
