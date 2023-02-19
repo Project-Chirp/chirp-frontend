9f5975d0162dc9eab0a8100b9727ba4338ea25df
NasihNazeem <nasihnazeem@gmail.com>
2023-02-19 10:46:13 -0700

    Updated styles import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./Styles/Theme";
+import theme from "./styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
