49d4f7550bfff9838e60999e14026152dd19ba56
NasihNazeem <nasihnazeem@gmail.com>
2023-02-19 10:36:00 -0700

    updated some of the imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./styles/Theme";
+import theme from "./Styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
