6cca33fd7714e674337626ee85d1a93d825ba85f
Dennull <lieudennis7@gmail.com>
2023-03-08 20:29:31 -0700

    Show main app only if user is logged in

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import Welcome from "./pages/Welcome.tsx";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Register from "./pages/Register";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated) {
+    return <Welcome />;
+  }
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<div>Home</div>} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
