6de4b368c0932a4d1adb2dfe1ffbe4d939f3845a
NasihNazeem <nasihnazeem@gmail.com>
2023-03-22 14:55:45 -0600

    Positioning of modal complete, ready for review

--- src/components/Misc/PostButtonModal.tsx
+++ src/components/Misc/PostButtonModal.tsx
@@ -1,6 +1,13 @@
 import React, { Dispatch, ReactElement, SetStateAction } from "react";
 import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material/";
 
+const styles = {
+  dialogPosition: {
+    position: "absolute",
+    bottom: "500px",
+  },
+};
+
 type ModalProps = {
   children: ReactElement<any, any>;
   openModal: boolean;
@@ -10,7 +17,18 @@ type ModalProps = {
 export default function PostButtonModal(props: ModalProps) {
   const { children, openModal, setOpenModal } = props;
   return (
-    <Dialog open={openModal}>
+    <Dialog
+      open={openModal}
+      fullWidth
+      scroll="paper"
+      sx={styles.dialogPosition}
+      PaperProps={{
+        style: {
+          minHeight: "20%",
+          maxHeight: "90%",
+        },
+      }}
+    >
       <DialogTitle>
         <Button color="greyButton" onClick={() => setOpenModal(!openModal)}>
           X
