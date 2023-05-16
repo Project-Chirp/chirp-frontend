04a230fa17a3702ca9104bc83413a81bd8168e53
NasihNazeem <nasihnazeem@gmail.com>
2023-05-16 17:02:52 -0600

    remove unused import

---
+++ src/pages/ExpandedPost.tsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+import { Stack } from "@mui/system";
+import PostList from "../components/Posts/PostList";
+
+const styles = {
+  root: {
+    width: "100%",
+  },
+  postListContainer: { padding: 2, width: "50%" },
+};
+
+const ExpandedPost = () => {
+  return (
+    <Stack direction="row" sx={styles.root}>
+      <Box sx={styles.postListContainer}>
+        {/* <ComposePost /> */}
+        <PostList />
+      </Box>
+    </Stack>
+  );
+};
+
+export default ExpandedPost;
