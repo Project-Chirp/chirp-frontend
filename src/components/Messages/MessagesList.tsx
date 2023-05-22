7a5cabb8458c3bf0db11311fb1c1fa324090eb8e
Dennull <lieudennis7@gmail.com>
2023-05-22 16:17:07 -0600

    Make latest messages query user specific

--- src/components/Messages/MessagesList.tsx
+++ src/components/Messages/MessagesList.tsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import MessagesListItem, { LatestMessageDetails } from "./MessagesListItem";
+import { useAppSelector } from "../../state/hooks";
 
 const MessagesList = () => {
+  const user = useAppSelector((state) => state.user);
   const [latestMessageDetails, setlatestMessageDetails] = useState<
     LatestMessageDetails[]
   >([]);
 
   useEffect(() => {
     const fetchMessages = async () => {
-      const result = await axios.get("http://localhost:3001/api/messages");
+      const result = await axios.get("http://localhost:3001/api/messages", {
+        params: {
+          userId: user.userId,
+        },
+      });
       setlatestMessageDetails(result.data as LatestMessageDetails[]);
     };
     fetchMessages();
-  }, []);
+  }, [user]);
 
   return (
     <>
