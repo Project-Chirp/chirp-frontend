6b8868430d309e11fc69c00e350a8ab3655f57bb
Dennull <lieudennis7@gmail.com>
2023-07-15 15:10:29 -0600

    Make navbar sticky

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,9 +3,10 @@ import NavBar from "../components/NavBar/NavBar";
 import { Box, Divider, Stack } from "@mui/material";
 
 const styles = {
-  container: { height: "100%", justifyContent: "center" },
+  container: { height: "auto", justifyContent: "center" },
+  divider: { height: "auto" },
   mainContent: { flex: "0 1 600px" },
-  nav: { flex: "0 0 275px" },
+  nav: { flex: "0 0 275px", height: "100vh", position: "sticky", top: 0 },
   secondaryContent: {
     flex: "0 0 350px",
     paddingLeft: "40px",
@@ -22,7 +23,7 @@ const Layout = ({ mainContent, secondaryContent }: LayoutProps) => {
     <Stack
       direction="row"
       sx={styles.container}
-      divider={<Divider orientation="vertical" />}
+      divider={<Divider orientation="vertical" sx={styles.divider} />}
     >
       <Box component="header" sx={styles.nav}>
         <NavBar />
