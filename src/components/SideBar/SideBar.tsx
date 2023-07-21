f5bdf2a5579a2ec61dfdb89e21a17e94766ea747
NasihNazeem <nasihnazeem@gmail.com>
2023-07-21 17:35:00 -0600

    advertisement done and footer complete

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 import React from "react";
 import SearchBar from "../Common/SearchBar";
 
@@ -31,11 +31,16 @@ const styles = {
     backgroundColor: "gray.light",
     borderRadius: 5,
     marginTop: 0,
+    boxSizing: "border-box",
+    border: "5px solid",
+    borderColor: "primary.main",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   footerContainer: {
     width: "100%",
     height: "5%",
-    backgroundColor: "gray.light",
   },
   mfHeader: {
     width: "100%",
@@ -52,6 +57,25 @@ const styles = {
     margin: 1,
   },
   friendItem: {},
+  adTitle: {
+    fontWeight: "bold",
+    color: "primary.main",
+    textAlign: "center",
+  },
+  footerNav: {
+    paddingX: 2,
+    height: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  footerItem: {
+    cursor: "pointer",
+    paddingRight: "0.75rem",
+    color: "gray.main",
+  },
+  footerItemText: {
+    fontSize: "0.75rem",
+  },
 };
 
 const SideBar = () => {
@@ -70,8 +94,45 @@ const SideBar = () => {
           <Box sx={styles.friendItem}></Box>
         </Box>
       </Box>
-      <Box sx={styles.adSpaceContainer}></Box>
-      <Box sx={styles.footerContainer}></Box>
+      <Box sx={styles.adSpaceContainer}>
+        <Typography variant="h4" sx={styles.adTitle}>
+          Your Advertisement Here
+        </Typography>
+      </Box>
+      <Box sx={styles.footerContainer}>
+        <Box sx={styles.footerNav}>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              Terms of Service
+            </Typography>
+          </Link>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              Privacy Policy
+            </Typography>
+          </Link>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              About Us
+            </Typography>
+          </Link>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              Contact Us
+            </Typography>
+          </Link>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              GitHub Repo
+            </Typography>
+          </Link>
+          <Link underline="hover" sx={styles.footerItem}>
+            <Typography variant="subtitle2" sx={styles.footerItemText}>
+              Documentation
+            </Typography>
+          </Link>
+        </Box>
+      </Box>
     </Box>
   );
 };
