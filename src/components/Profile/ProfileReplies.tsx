14b1ad2ed520f79985127dc0b5f5089f97779061
NasihNazeem <nasihnazeem@gmail.com>
2024-04-20 19:06:50 -0600

    introduced react query and intersection observer

--- src/components/Profile/ProfileReplies.tsx
+++ src/components/Profile/ProfileReplies.tsx
@@ -5,32 +5,71 @@ import { useAppDispatch, useAppSelector } from "../../state/hooks";
 import { Post, setPosts } from "../../state/slices/postsSlice";
 import { Divider, Stack } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 
 const ProfileReplies = () => {
   const { username } = useParams();
   const { posts } = useAppSelector((state) => state.posts);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
+  const fetchPosts = async ({ pageParam = 1 }) => {
+    try {
       const result = await axios.get(
         "http://localhost:3001/api/profile/getUserReplies",
         {
           params: {
             username,
+            offset: pageParam,
           },
         }
       );
-      dispatch(setPosts(result.data as Post[]));
-    };
-    fetchPosts();
-  }, [dispatch, username]);
 
-  return (
+      if (pageParam > 1) {
+        dispatch(setPosts([...posts, ...result.data] as Post[]));
+      } else {
+        dispatch(setPosts(result.data as Post[]));
+      }
+
+      return result.data;
+    } catch (error) {
+      console.error("error fetching posts:", error);
+    }
+  };
+
+  const { data, error, status, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["query"],
+      queryFn: fetchPosts,
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.length ? allPages.length + 1 : undefined;
+      },
+    });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
+  useEffect(() => {
+    fetchPosts({ pageParam: 1 });
+  }, [username]);
+
+  return status === "pending" ? (
+    <div>Loading...</div>
+  ) : status === "error" ? (
+    <div>{error.message}</div>
+  ) : (
     <Stack divider={<Divider />}>
       {posts.map((o, index) => (
         <PostItem key={index} post={o} />
       ))}
+
+      <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
       <Divider />
     </Stack>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "./styles/Theme";
 import AuthProvider from "./components/Auth/AuthProvider";
 import { Provider } from "react-redux";
@@ -12,14 +13,18 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+              <App />
+            </ThemeProvider>
+          </QueryClientProvider>
         </Provider>
       </AuthProvider>
     </BrowserRouter>
