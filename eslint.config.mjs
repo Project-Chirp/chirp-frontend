84e6a1f39fc8cee70f3d372e7fdd8bf26ddd63b5
Dennull <lieudennis7@gmail.com>
2024-05-08 21:36:21 -0600

    Configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -11,6 +12,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  eslintConfigPrettier,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
