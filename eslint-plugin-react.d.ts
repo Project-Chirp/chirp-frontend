67de5b1696205a192f3f5a49d772b8fd406f43f1
Dennull <lieudennis7@gmail.com>
2024-05-08 21:13:17 -0600

    Configure ESLint

---
+++ eslint-plugin-react.d.ts
@@ -0,0 +1,4 @@
+declare module "eslint-plugin-react/configs/recommended.js" {
+  const config: {};
+  export default config;
+}
---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+/// <reference path="eslint-plugin-react.d.ts" />
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
