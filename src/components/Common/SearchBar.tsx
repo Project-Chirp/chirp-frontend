85cdb3ec54de7276b71ec57ffc5d98a9aa639f04
NasihNazeem <nasihnazeem@gmail.com>
2024-05-13 19:53:07 -0600

    updated searchbar to be similar to messagessearchbar

--- src/components/Common/SearchBar.tsx
+++ src/components/Common/SearchBar.tsx
@@ -1,25 +1,123 @@
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { InputAdornment, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Avatar,
+  Box,
+  IconButton,
+  InputAdornment,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { User } from "../../state/slices/userSlice";
+import axios from "axios";
 
 type SearchBarProps = {
   placeholder: string;
+  onSearchOpen: () => void;
+  onSearchClose: () => void;
+  onSelect: (state?: number) => void;
 };
 
-const SearchBar = ({ placeholder }: SearchBarProps) => {
+const styles = {
+  autocomplete: { "&.MuiAutocomplete-input": { paddingLeft: 0 } },
+  box: {
+    paddingBottom: 1,
+    paddingTop: 0,
+    paddingX: 2,
+  },
+  displayName: {
+    fontWeight: "bold",
+  },
+  searchIcon: { paddingRight: 0 },
+};
+
+const SearchBar = ({
+  placeholder,
+  onSearchClose,
+  onSearchOpen,
+  onSelect,
+}: SearchBarProps) => {
+  const [keywords, setKeywords] = useState("");
+  const [userList, setUserList] = useState<User[]>([]);
+
+  const fetchUsers = async ({ pageParam = 1 }) => {
+    const result = await axios.get("http://localhost:3001/api/users/getUsers", {
+      params: {
+        keyword: keywords,
+        offset: pageParam,
+      },
+    });
+    setUserList(result.data as User[]);
+  };
+
+  useEffect(() => {
+    fetchUsers({ pageParam: 1 });
+  }, [keywords]);
+
   return (
-    <TextField
-      fullWidth
-      hiddenLabel
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchRoundedIcon />
-          </InputAdornment>
-        ),
-      }}
-      placeholder={placeholder}
-      size="small"
-    />
+    <Box sx={styles.box}>
+      <Autocomplete
+        fullWidth
+        getOptionLabel={(option) => `${option.displayName} @${option.username}`}
+        id="messages-search"
+        popupIcon={false}
+        onOpen={onSearchOpen}
+        onClose={onSearchClose}
+        options={userList}
+        openOnFocus
+        renderInput={(params) => {
+          return (
+            <TextField
+              {...params}
+              onChange={(word) => setKeywords(word.target.value)}
+              fullWidth
+              hiddenLabel
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton disabled sx={styles.searchIcon}>
+                      <SearchRoundedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              placeholder={placeholder}
+              size="small"
+            />
+          );
+        }}
+        renderOption={(_, option) => {
+          return (
+            <ListItemButton
+              key={option.userId}
+              component="li"
+              onClick={() => onSelect(option.userId)}
+            >
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
+              <ListItemText
+                disableTypography
+                primary={
+                  <Box>
+                    <Typography sx={styles.displayName} variant="body2">
+                      {option.displayName}
+                    </Typography>
+                    <Typography variant="body2">{`@${option.username}`}</Typography>
+                  </Box>
+                }
+              />
+            </ListItemButton>
+          );
+        }}
+        sx={styles.autocomplete}
+      />
+    </Box>
   );
 };
 
--- src/state/slices/userSlice.ts
+++ src/state/slices/userSlice.ts
@@ -1,6 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-type User = {
+export type User = {
   isLoading: boolean;
   userId?: number;
   username?: string;
