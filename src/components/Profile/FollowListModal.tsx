6c5a4eb2c63b52801b69471d8d54b53d7de38c57
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-06-17 22:35:25 -0600

    WIP - Rough Mockup of how the Follow List Should Work

--- src/components/Profile/FollowListModal.tsx
+++ src/components/Profile/FollowListModal.tsx
@@ -7,10 +7,21 @@ import {
   DialogContent,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { User } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+
+type user = {
+  userName: string;
+  displayName: string;
+  imageURL: string;
+  isFollowing: boolean;
+};
 
 type FollowListModalProps = {
   openModal: boolean;
   onClose: () => void;
+  listType: "Followers | Following";
+  fetchList: () => Promise<user[]>;
 };
 
 const styles = {
@@ -28,7 +39,17 @@ const styles = {
 export default function FollowListModal({
   openModal,
   onClose,
+  listType,
+  fetchList,
 }: FollowListModalProps) {
+  const [list, setList] = useState<User[]>([]);
+
+  useEffect(() => {
+    if (openModal) {
+      fetchList().then(setList).catch(console.error);
+    }
+  }, [openModal, listType, fetchList]);
+
   return (
     <Dialog
       fullWidth
@@ -38,13 +59,19 @@ export default function FollowListModal({
       PaperProps={{ sx: styles.dialog }}
     >
       <DialogTitle sx={styles.header}>
-        Followers
+        {listType}
         <IconButton onClick={onClose}>
           <CloseIcon />
         </IconButton>
       </DialogTitle>
 
-      <DialogContent>{/* Additional stuff goes here */}Whats up</DialogContent>
+      <DialogContent>
+        Erm What the Sigma
+        {/* Need to create the endpoint to get the user list */}
+        {/* {list.map((item) => (
+          <Typography key={item.id}>{item.username}</Typography>
+        ))} */}
+      </DialogContent>
     </Dialog>
   );
 }
