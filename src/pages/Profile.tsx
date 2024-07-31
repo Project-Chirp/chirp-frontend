c410f914ba4701e9d6855d20fe205281cbec9f04
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-07-31 01:55:30 -0600

    Updated endpoints url based on backend changes

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -487,7 +487,7 @@ const Profile = () => {
     setFollowListModelOpen(true);
     try {
       setFollowListModalLoading(true);
-      const endpoint = "http://localhost:3001/api/follow/getFollowersUserList";
+      const endpoint = "http://localhost:3001/api/follow/getFollowersList";
       const result = await axios.get(endpoint, {
         params: {
           visitedUserId: profileContents.userId,
@@ -507,7 +507,7 @@ const Profile = () => {
     setFollowListModelOpen(true);
     try {
       setFollowListModalLoading(true);
-      const endpoint = "http://localhost:3001/api/follow/getFollowingUserList";
+      const endpoint = "http://localhost:3001/api/follow/getFollowingList";
       const result = await axios.get(endpoint, {
         params: {
           visitedUserId: profileContents.userId,
