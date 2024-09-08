725e013b21f22e2d5de2e68fa9a6b94823b03cc9
Dennull <lieudennis7@gmail.com>
2024-09-07 23:32:30 -0600

    More markup and styling changes for follower list

--- src/components/Common/FollowButton.tsx
+++ src/components/Common/FollowButton.tsx
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import { Button } from "@mui/material";
-import axios from "axios";
-import { useAppSelector } from "../../state/hooks";
-
-const styles = {
-  followButton: {
-    boxShadow: "none",
-    fontWeight: "bold",
-    minWidth: "84px",
-    "&:hover": {
-      boxShadow: "none",
-    },
-  },
-};
-
-type FollowButtonProps = {
-  onClick?: () => void;
-  visitedUserId: number;
-};
-
-const FollowButton = ({ onClick, visitedUserId }: FollowButtonProps) => {
-  const user = useAppSelector((state) => state.user);
-
-  const handleFollow = async (e: React.MouseEvent) => {
-    e.stopPropagation();
-    try {
-      await axios.put(
-        "http://localhost:3001/api/follow/followUser",
-        {
-          currentUserId: user.userId,
-          visitedUserId: visitedUserId,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      onClick?.();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <Button
-      onClick={handleFollow}
-      size="small"
-      sx={styles.followButton}
-      variant="contained"
-    >
-      Follow
-    </Button>
-  );
-};
-
-export default FollowButton;
-=======
 import { Button } from "@mui/material";
 import axios from "axios";
 import { useAppSelector } from "../../state/hooks";
@@ -114,4 +55,3 @@ const FollowButton = ({ onClick, visitedUserId }: FollowButtonProps) => {
 };
 
 export default FollowButton;
->>>>>>> 518ed42 (added many features to modal such as navigation, styling, follow/unfollow, update the following count of active user)
--- src/components/Common/FollowingButton.tsx
+++ src/components/Common/FollowingButton.tsx
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-import { Button } from "@mui/material";
-import { useAppSelector } from "../../state/hooks";
-import axios from "axios";
-import { useState } from "react";
-
-const styles = {
-  followingButton: {
-    backgroundColor: "white.main",
-    border: 1,
-    boxShadow: "none",
-    color: "primary.main",
-    fontWeight: "bold",
-    minWidth: "84px",
-    "&:hover": {
-      boxShadow: "none",
-      backgroundColor: "white.main",
-      border: 1,
-      borderColor: "error.main",
-      color: "error.main",
-    },
-  },
-};
-
-type FollowingButtonProps = {
-  onClick?: () => void;
-  visitedUserId: number;
-};
-
-const FollowingButton = ({ onClick, visitedUserId }: FollowingButtonProps) => {
-  const user = useAppSelector((state) => state.user);
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleUnfollow = async (e: React.MouseEvent) => {
-    e.stopPropagation();
-
-    try {
-      await axios.put(
-        "http://localhost:3001/api/follow/unfollowUser",
-        {
-          currentUserId: user.userId,
-          visitedUserId: visitedUserId,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      onClick?.();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <Button
-      onClick={handleUnfollow}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      size="small"
-      sx={styles.followingButton}
-      variant="contained"
-    >
-      {isHovered ? "Unfollow" : "Following"}
-    </Button>
-  );
-};
-
-export default FollowingButton;
-=======
 import { Button } from "@mui/material";
 import { useAppSelector } from "../../state/hooks";
 import axios from "axios";
@@ -138,4 +67,3 @@ const FollowingButton = ({ onClick, visitedUserId }: FollowingButtonProps) => {
 };
 
 export default FollowingButton;
->>>>>>> 518ed42 (added many features to modal such as navigation, styling, follow/unfollow, update the following count of active user)
